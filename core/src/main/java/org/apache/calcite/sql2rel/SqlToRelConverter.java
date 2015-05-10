7bfed7f7d984cd7c3025999bf06a41225aec7380
Aman Sinha <asinha@maprtech.com>
2015-05-09 17:57:23 -0700

    Lower the IN list threshold to 10.

--- core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -1358,10 +1358,10 @@ public class SqlToRelConverter {
    * predicate. A threshold of 0 forces usage of an inline table in all cases; a
    * threshold of Integer.MAX_VALUE forces usage of OR in all cases
    *
-   * @return threshold, default 200
+   * @return threshold, default 10
    */
   protected int getInSubqueryThreshold() {
-    return 200;
+    return 10;
   }
 
   /**
